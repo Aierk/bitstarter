e98e549549d8a479bed66e42fe4888eb8fd85b88
Sergio A. Morales <smorales@inf.utfsm.cl>
2013-07-09 12:12:44 -0400

    Updated web.js read 03

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 var respuesta;
-
+var fs = require('fs');
 fs.readFile('index.html', function (err, data) {
   if (err) throw err;
   respuesta = data.toString();
